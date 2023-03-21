a49172a0bd1b4d5df1b00ea8a9d16dbf56d9c433
LauraKaraliene <Laura.Karaliene@stud.noroff.no>
2023-03-21 19:56:34 +0100

    2023-03-21
    some changes with javascript

--- js/productsJs/redWomanJacket.js
+++ js/productsJs/redWomanJacket.js
@@ -1,6 +1,10 @@
 const clickableElements = document.querySelectorAll(".clickable");
 const clickableLetters = document.querySelectorAll(".clickable-letter");
+const button = document.querySelector(".cta_add-to-cart");
+const cartNumber = document.querySelector(".cart-icon-number");
+let number = localStorage.getItem("cartCount") || 0;
 
+//clickable color and size rectangles
 clickableElements.forEach((element) => {
   element.addEventListener("click", () => {
     clickableElements.forEach((otherElement) => {
@@ -22,3 +26,67 @@ clickableLetters.forEach((element) => {
     element.classList.add("clickable-letter");
   });
 });
+
+//shopping cart icon update
+cartNumber.textContent = number.toString();
+
+window.addEventListener("storage", function (event) {
+  if (event.key === "cartCount") {
+    number = parseInt(event.newValue);
+    cartNumber.textContent = number.toString();
+  }
+});
+
+button.addEventListener("click", function () {
+  number++;
+
+  cartNumber.textContent = number.toString();
+  localStorage.setItem("cartCount", number);
+});
+
+// button.onclick = addToCart;
+
+// function addToCart() {
+//   number++;
+//   localStorage.setItem("cartItems", number);
+//   for(var i=0, i < number, i++) {
+
+//   }
+// };
+
+// if(localStorage.getItem("cartItems")) {
+//   number = localStorage.getItem("cartItems");
+//   for(let i = 0, i < number, i++){
+// cartContent.innerHTML += `  <section class="main-content">
+// <div class="main-left-middle-block">
+
+//   <div class="main-left-jacket-block">
+//     <div>
+//       <a href="../products/red-women-jacket.html"
+//         ><img
+//           src="../images/red_women_jacket.png"
+//           alt="red women jacket"
+//           class="main-left-jacket"
+//       /></a>
+//     </div>
+//     <div>
+//       <h4>Red Women Jacket</h4>
+//       <p>Size: M</p>
+//       <p>Colour: Red</p>
+//     </div>
+//     <div class="main-middle-block">
+//       <div class="plus-minus">
+//         <button class="button-minus">-</button>
+//         <p>1</p>
+//         <button class="button-plus">+</button>
+//       </div>
+//       <p class="main-middle-price">nok 2300</p>
+//       <p class="remove">Remove</p>
+//     </div>
+//   </div>`;
+// }
+// }
+
+// function addToCart() {
+//   button.onclick = addToCart(item);
+// }
