9337afdd0b0028ab2c4dfa41c38c3d806d14f4ba
LauraKaraliene <Laura.Karaliene@stud.noroff.no>
2023-03-21 14:50:02 +0100

    2023-03-21

--- js/productsJs/jackets.js
+++ js/productsJs/jackets.js
@@ -6,7 +6,7 @@ jackets.forEach((jacket) => {
   productList.innerHTML += `<div>
                               <a href="red-women-jacket.html">
                                  <img src=${jacket.imgSrc}
-                              alt="red women jacket" class="main-img"/>
+                              alt="${jacket.name}" class="main-img"/>
                               <h3>${jacket.name}</h3>
                               <h4>Nok ${jacket.price}</h4></a>
                             </div>`;
