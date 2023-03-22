b62015cafd4b795e1f473d0dd04d26481e0ac088
LauraKaraliene <Laura.Karaliene@stud.noroff.no>
2023-03-22 19:44:12 +0100

    2023-03-22
    changed cart style,
    added popup coontainer on jacket page

--- js/productsJs/redWomanJacket.js
+++ js/productsJs/redWomanJacket.js
@@ -2,6 +2,12 @@ const clickableElements = document.querySelectorAll(".clickable");
 const clickableLetters = document.querySelectorAll(".clickable-letter");
 const button = document.querySelector(".cta_add-to-cart");
 const cartNumber = document.querySelector(".cart-icon-number");
+const popup = document.querySelector(".popup");
+const cartIcon = document.querySelector(".shopping_cart");
+let subtotalPrice = parseFloat(
+  document.querySelector(".subtotal-price").innerText
+);
+
 let number = localStorage.getItem("cartCount") || 0;
 
 //clickable color and size rectangles
@@ -37,11 +43,22 @@ window.addEventListener("storage", function (event) {
   }
 });
 
-button.addEventListener("click", function () {
+button.addEventListener("click", () => {
   number++;
+  popup.classList.add("show");
+  popup.style.display = "block";
 
-  cartNumber.textContent = number.toString();
-  localStorage.setItem("cartCount", number);
+  setTimeout(() => {
+    cartNumber.textContent = number.toString();
+    localStorage.setItem("cartCount", number);
+
+    popup.style.display = "none";
+    popup.classList.remove("show");
+  }, 4000);
+  // subtotalPrice += 2300;
+
+  // document.querySelector(".subtotal-price").innerText =
+  //   subtotalPrice.toFixed(2);
 });
 
 // button.onclick = addToCart;
