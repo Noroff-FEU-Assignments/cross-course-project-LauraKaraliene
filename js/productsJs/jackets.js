0ff2201f05371e7c183afb2bb5d378e2697dfa81
LauraKaraliene <Laura.Karaliene@stud.noroff.no>
2023-04-30 18:44:17 +0200

    2023-04-30
    added products from wp to jackets page

--- js/productsJs/jackets.js
+++ js/productsJs/jackets.js
@@ -1,13 +1,32 @@
-import jackets from "./products.js";
-
+const url = "http://localhost:10014/wp-json/wc/store/products?&per_page=15";
 const productList = document.querySelector(".product-list");
+const loading = document.querySelector(".loading");
 
-jackets.forEach((jacket) => {
-  productList.innerHTML += `<div>
-                              <a href="red-women-jacket.html">
-                                 <img src=${jacket.imgSrc}
-                              alt="${jacket.name}" class="main-img"/>
-                              <h3>${jacket.name}</h3>
-                              <h4>Nok ${jacket.price}</h4></a>
+async function getProducts() {
+  loading.classList.add("loading");
+  productList.innerHTML = "";
+  try {
+    const response = await fetch(url);
+    const getResults = await response.json();
+    createHTML(getResults);
+  } catch (error) {
+    console.log(error);
+    // productList.innerHTML = alertMessage(error, "An error occurred");
+  } finally {
+    loading.classList.remove("loading");
+  }
+}
+
+getProducts();
+
+function createHTML(products) {
+  products.forEach(function (product) {
+    productList.innerHTML += `<div>
+                              <a href="specific-jacket.html?post=${product.post}">
+                                 <img src=${product.images[0].src}
+                              alt="${product.name}" class="main-img"/>
+                              <h3>${product.name}</h3>
+                              <h4>Nok ${product.prices.price}</h4></a>
                             </div>`;
-});
+  });
+}
--- js/productsJs/specificJacket.js
+++ js/productsJs/specificJacket.js
@@ -7,8 +7,62 @@ const cartIcon = document.querySelector(".shopping_cart");
 let subtotalPrice = parseFloat(
   document.querySelector(".subtotal-price").innerText
 );
-
 let number = localStorage.getItem("cartCount") || 0;
+
+const productPage = document.querySelector(".product-page");
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+const url = "http://localhost:10014/wp-json/wc/store/products/";
+
+async function fetchProduct() {
+  try {
+    const response = await fetch(url);
+    const products = await response.json();
+    createHTML(products);
+  } catch (error) {
+    // productPage.innerHTML = alertMessage(error, "An error occurred");
+  }
+}
+
+fetchProduct();
+
+function createHTML(products) {
+  productPage.innerHTML = `<section class="product-page">
+  <section class="jacket">
+  <div>
+  <ul class="breadcrumbs">
+    <li><a href="../index.html">Home/ </a></li>
+    <li><a href="../women.html">Women/ </a></li>
+    <li><a href="jackets.html">Outlet/</a></li>
+    <li><a href="red-women-jacket.html">Red Women Jacket </a></li>
+  </ul>
+</div>
+<div>
+  <img src=${products.images[0].src}
+  alt="${products.name}" class="product_specific"/>
+</div>
+<div class="jacket-small-block">
+<img src=${products.images[0].src}
+  alt="${products.name}" class="jacket-small"/>
+  <img src=${products.images[0].src}
+  alt="${products.name}" class="jacket-small"/>
+  <img src=${products.images[0].src}
+  alt="${products.name}" class="jacket-small"/>
+  <img src=${products.images[0].src}
+  alt="${products.name}" class="jacket-small"/>
+</div>
+</section>
+<section class="jacket-info">
+<div class="info">
+  <h1>${products.name}</h1>
+  <h2>Nok ${products.prices.price}</h2>
+</div>
+<div class="description info">
+  <p>${products.description}</p>
+</div>`;
+}
 
 //clickable color and size rectangles
 clickableElements.forEach((element) => {
