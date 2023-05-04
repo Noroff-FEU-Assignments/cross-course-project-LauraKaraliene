bfaf6e778b496fd8d3bf1e1a26fba09c96c468ab
LauraKaraliene <Laura.Karaliene@stud.noroff.no>
2023-05-04 17:40:35 +0200

    2023-05-04
    changed url

--- js/productsJs/specificJacket.js
+++ js/productsJs/specificJacket.js
@@ -12,15 +12,15 @@ let number = localStorage.getItem("cartCount") || 0;
 const productPage = document.querySelector(".product-page");
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
-const id = params.get("id");
+const post = params.get("post");
 
-const url = "http://localhost:10014/wp-json/wc/store/products/";
+const url = "http://rainydaysss.icu/wp-json/wc/store/products?post=" + post;
 
 async function fetchProduct() {
   try {
     const response = await fetch(url);
-    const products = await response.json();
-    createHTML(products);
+    const product = await response.json();
+    createHTML(product);
   } catch (error) {
     // productPage.innerHTML = alertMessage(error, "An error occurred");
   }
@@ -28,7 +28,7 @@ async function fetchProduct() {
 
 fetchProduct();
 
-function createHTML(products) {
+function createHTML(product) {
   productPage.innerHTML = `<section class="product-page">
   <section class="jacket">
   <div>
@@ -40,27 +40,27 @@ function createHTML(products) {
   </ul>
 </div>
 <div>
-  <img src=${products.images[0].src}
-  alt="${products.name}" class="product_specific"/>
+  <img src=${product.images[0].src}
+  alt="${product.name}" class="product_specific"/>
 </div>
 <div class="jacket-small-block">
-<img src=${products.images[0].src}
-  alt="${products.name}" class="jacket-small"/>
-  <img src=${products.images[0].src}
-  alt="${products.name}" class="jacket-small"/>
-  <img src=${products.images[0].src}
-  alt="${products.name}" class="jacket-small"/>
-  <img src=${products.images[0].src}
-  alt="${products.name}" class="jacket-small"/>
+<img src=${product.images[0].src}
+  alt="${product.name}" class="jacket-small"/>
+  <img src=${product.images[0].src}
+  alt="${product.name}" class="jacket-small"/>
+  <img src=${product.images[0].src}
+  alt="${product.name}" class="jacket-small"/>
+  <img src=${product.images[0].src}
+  alt="${product.name}" class="jacket-small"/>
 </div>
 </section>
 <section class="jacket-info">
 <div class="info">
-  <h1>${products.name}</h1>
-  <h2>Nok ${products.prices.price}</h2>
+  <h1>${product.name}</h1>
+  <h2>Nok ${product.prices.price}</h2>
 </div>
 <div class="description info">
-  <p>${products.description}</p>
+  <p>${product.description}</p>
 </div>`;
 }
 
